6059f76355bb25741664a0642852b7f6bad72db4
satya-sigmoid <satya1@sigmoidanalytics.com>
2023-07-15 10:59:37 +0530

    println change

--- string/basicString.java
+++ string/basicString.java
@@ -4,7 +4,7 @@ public class basicString {
     public static void main(String[] args) {
         String name1 = "Monu";
         for (int i = 0; i < name1.length(); i++) {
-            System.out.print(name1.charAt(i));
+            System.out.print(name1.charAt(i)+" ");
         }
     }
 }
